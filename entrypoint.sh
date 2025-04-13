a70173cfd3e0d00a402104a255010b55ed2511d3
Einer Bravo <einerbravo@gmail.com>
2025-04-12 21:29:20 -0500

    fix: modify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,5 @@ echo "Python path: $PYTHONPATH" >&2
 echo "Contenido de /app:" >&2
 ls -la /app >&2
 
-# Run migrations
-cd /app && alembic upgrade head
-
 # Run server
 exec uvicorn app.main:app --host 0.0.0.0 --port 8080
